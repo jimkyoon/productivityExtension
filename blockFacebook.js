a730da6d151321bfbf744335f3a6036e9faf0d04
Jim Yoon <jimyoon90@gmail.com>
2017-05-05 13:24:45 -0700

    added facebook blocking

---
+++ blockFacebook.js
@@ -0,0 +1,11 @@
+var contentStrip = $('#stream_pagelet');
+
+var contentParent = contentStrip.parent();
+
+contentParent.remove();
+
+// contentParent.prepend(<h1>GET BACK TO WORK!!!</h1>);
+//
+// var textElement = contentParent.children()[0];
+//
+// textElement.addClass('beauText');
